6536ad5658c018df694ae00c27acde9ab5e78ef6
Neo <neo.gkbeauty@gmail.com>
2025-11-11 00:34:07 +0700

    build game web

---
+++ scripts/coin.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+@onready var game_manager = %GameManager
+@onready var anim = $AnimationPlayer
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		game_manager.add_point()
+		anim.play("pickup")
